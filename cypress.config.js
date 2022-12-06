a1f47e517f6d6950b3e0ce5885f92cf8617636ae
Quargs Greene <quargsgreene@gmail.com>
2022-12-06 15:42:39 -0800

    tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://127.0.0.1:8887',
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config);
 
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,6 @@
+/* eslint-disable indent */
 describe('tchnouodt52212012 browser tests', () => {
   it('visits the page', () => {
-    cy.visit('https://quargsgreene.github.io/tchnouodt52212012/');
-  });
-
-  it('plays a note on the synthesizer when clicked', () => {
-    cy.get('.synthesizer').click({ multiple: true });
+    cy.visit('http://127.0.0.1:8887');
   });
 });
