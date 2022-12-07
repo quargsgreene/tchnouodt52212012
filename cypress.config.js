4b8841ed46b63a4dbb96a6339e72d379f0f03228
Quargs Greene <quargsgreene@gmail.com>
2022-12-06 16:38:42 -0800

    tests

--- cypress.config.js
+++ cypress.config.js
@@ -4,13 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:8887',
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config);
-
-      // add other tasks to be registered here
-
-      // IMPORTANT to return the config object
-      // with the any changed environment variables
-      return config;
     },
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,4 +3,8 @@ describe('tchnouodt52212012 browser tests', () => {
   it('visits the page', () => {
     cy.visit('http://127.0.0.1:8887');
   });
+
+  it('finds buttons', () => {
+    cy.get('.synthesizer', { multiple: true });
+  });
 });
